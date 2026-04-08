b39ff0c42ce8f5c2c854926eeca65bdac8f6b3bc
Jhyun02 <hjh75035117@gmail.com>
2026-04-08 17:37:47 +0900

    feat: initialize spring boot project

---
+++ hsa-core/src/test/java/com/example/hsa_core/HsaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsa_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
